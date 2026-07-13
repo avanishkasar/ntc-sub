6540dfe97b176cd8aa8e503b69f5a7c2c0ce60d5
avanishkasar <avanishkasar@gmail.com>
2026-07-13 21:27:00 +0530

    p06: swap with third variable — 3-step logic nailed

---
+++ ntc sub/programs/p06_swap_with_temp.py
@@ -0,0 +1,7 @@
+num1 = int(input("give your 1st number: "))
+num2 = int(input("give your 2nd number: "))
+num3 = num1
+print(f"lets assume num1 to be num3 = {num3}")
+num1 = num2
+num2 = num3
+print(f"you swapped numbers are = {num1} {num2}")
