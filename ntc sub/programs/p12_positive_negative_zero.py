b239e6e7906b5332d57aa7a7c6de52ac4f1ec1d1
avanishkasar <avanishkasar@gmail.com>
2026-07-18 11:28:25 +0530

    p12: positive, negative, zero - using elif

---
+++ ntc sub/programs/p12_positive_negative_zero.py
@@ -0,0 +1,7 @@
+num = int(input("give your number : "))
+if num > 0:
+    print(f"the number is positive")
+elif num == 0:
+    print(f"the number is zero/null")
+else:
+   print(f"the number is negative")
