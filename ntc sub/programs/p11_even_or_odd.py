e474f6dd94bcf4e41dcaad0b48c61d6f6c77b193
avanishkasar <avanishkasar@gmail.com>
2026-07-18 11:24:24 +0530

    p11: even or odd - completed with if/else

--- ntc sub/programs/p11_even_or_odd.py
+++ ntc sub/programs/p11_even_or_odd.py
@@ -1,22 +1,5 @@
-# Program 11 - Even or Odd
-# Attempt 1 (wrong logic - checking >= 0 instead of == 0)
-# num = int(input("give your number to test even odd : "))
-# num1 = num / 2
-# if num1 >= 0:
-#     print("your number is even")
-# else:
-#     print("its odd")
-
-# Attempt 2 (missing indentation + wrong condition)
-# num = int(input("give your number to test even odd : "))
-# if num % 2:
-# print("your number is even")
-# else:
-# print("its odd")
-
-# Still working on this one...
 num = int(input("give your number to test even odd : "))
-if num % 2 == 0:
-    print(f"your number is even")
+if num % 2 <= 0:       
+    print("your number is even")   
 else:
-    print(f"its odd")
+    print("its odd")
